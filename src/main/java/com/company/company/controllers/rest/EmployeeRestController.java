fa6d92d6bf4cdbd9acb103b7b6f8cd5fca3e6315
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2019-05-15 14:41:51 +0300

    complete rest for employee

--- src/main/java/com/company/company/controllers/rest/EmployeeRestController.java
+++ src/main/java/com/company/company/controllers/rest/EmployeeRestController.java
@@ -3,7 +3,12 @@ package com.company.company.controllers.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.company.company.model.entity.Employee;
@@ -25,4 +30,38 @@ public class EmployeeRestController {
 		return employeeService.findAll();
 	}
 	
+	@GetMapping(value = "employee/{employeeId}")
+	public Employee getEmployee(@PathVariable("employeeId") int emplId) {
+		
+		Employee employee = employeeService.findById(emplId);
+		
+		return employee;
+	}
+	
+	@PostMapping(value = "employee")
+	public Employee saveEmployee (@RequestBody Employee employee) {
+		
+		employee.setEmplId(0);
+		
+		employeeService.saveOrUpdateEmployee(employee);
+		
+		return employee;
+	}
+	
+	@PutMapping(value = "employee")
+	public Employee editEmployee (@RequestBody Employee employee) {
+		
+		employeeService.saveOrUpdateEmployee(employee);
+		
+		return employee;
+	}
+	
+	@DeleteMapping(value = "employee/{employeeId}")
+	public String deleteEmployee(@PathVariable("employeeId") int emplId) {
+		
+		employeeService.deleteEmployee(emplId);
+		
+		return "Deleted Employee with id -"+emplId;
+
+	}
 }
