487a31124f9b45abdc32d84801a11439a2bfe1fc
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2019-05-17 17:02:38 +0300

    Update LoginView.java

--- src/main/java/com/company/company/view/LoginView.java
+++ src/main/java/com/company/company/view/LoginView.java
@@ -2,19 +2,18 @@ package com.company.company.view;
 
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("")
+@Route("login")
 public class LoginView extends VerticalLayout {
 	
 	
 	
 	public LoginView()  {
 		
-		Button button = new Button("Click");
+		Button button = new Button("Save to click");
 		button.addClickListener(e -> Notification.show("Login is coming!!!"));
 		
 		add(button);
